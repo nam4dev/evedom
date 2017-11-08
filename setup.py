61d9f83c3bfed08dfaf7b11c187ea387da8dad59
Namgyal Brisson <namat4css@gmail.com>
2017-11-08 16:57:29 +0100

    [setup.py] Add classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Evedom Packaging Info
+Evedom Packaging Settings
 """
 
 __author__ = '(nam4dev) Namgyal Brisson'
@@ -14,13 +14,21 @@ setup(
     name='evedom',
     version='0.0.1',
     packages=['evedom', 'evedom.helpers'],
-    url='',
+    url='https://github.com/nam4dev/evedom',
     license='MIT',
     author='nam4dev',
     author_email='namat4css@gmail.com',
     description=(
-        'Allow to manage python-eve endpoint(s) in a structured manner' 
+        'Allow to manage python-eve endpoint(s) in a structured manner'
         'and to get all endpoint(s) automatically loaded'
     ),
     keywords='eve evedom',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
 )
